1084e7cbd79f7acb17463b27dde56e3a381b84bc
Dan Ott <thedanielott@gmail.com>
2014-04-07 09:01:55 -0700

    Give nodejs lower precedencein $PATH

--- node/path.bash
+++ node/path.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Node Pacakage Manger Executables (brew install node)
-export PATH="/usr/local/share/npm/bin:${PATH}"
+export PATH="${PATH}:/usr/local/share/npm/bin"
 export NODE_PATH="/usr/local/lib/node"
