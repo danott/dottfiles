a784abae0fd0a9619b651caea82e9fffacaead97
Dan Ott <thedanielott@gmail.com>
2014-04-07 09:12:18 -0700

    Consistency.

--- go/path.bash
+++ go/path.bash
@@ -1,4 +1,4 @@
-export GOPATH="${PROJECTS}/go"
-export PATH="${PATH}:${GOPATH}/bin"
+export GOPATH="$PROJECTS/go"
+export PATH="$PATH:$GOPATH/bin"
 export GOROOT=`go env GOROOT`
-export PATH=$PATH:$GOROOT/bin
+export PATH="$PATH:$GOROOT/bin"
