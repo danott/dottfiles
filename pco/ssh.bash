b4d2a26a4b3f835ae0cae3bafd943811d3ef8304
Dan Ott <thedanielott@gmail.com>
2014-06-08 16:40:38 -0700

    Keep 'em updated

--- pco/ssh.bash
+++ pco/ssh.bash
@@ -1,1 +1,4 @@
-ssh-add ~/.ssh/pco_servers > /dev/null 2>&1
+ssh-add -D > /dev/null 2>&1
+ssh-add $HOME/.ssh/pco_servers $HOME/.ssh/id_rsa > /dev/null 2>&1
+
+# export PULSAR_CONF_REPO="$HOME/Code/pco-deploy/"
