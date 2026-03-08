f9089c32964d22a9ee0853adbef13f9787aea0d1
Dan Ott <danott@users.noreply.github.com>
2026-03-08 00:58:40 -0500

    Avoid collissions for this rarely used reloading of hammerspoon

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -2,7 +2,7 @@ require("hs.ipc")
 require("ghostty-tmux-integration")
 
 -- Reload config
-hs.hotkey.bind({ "cmd", "shift" }, "r", function()
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "r", function()
   hs.reload()
 end)
 hs.alert.show("Hammerspoon loaded")
